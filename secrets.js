e36b2630c3bf4544041941f0e3f96df7b921ad9b
srackley <shelbyrackleyart@gmail.com>
2017-11-15 14:54:53 -0500

    separated client and added package.json

---
+++ secrets.js
@@ -0,0 +1,3 @@
+const API_SECRET = b4a5db3cf97a92b2a3e3a4ab6c43e5bc;
+
+module.exports = API_SECRET;
--- server/api/check.js
+++ server/api/check.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  // Comment out this line:
+  // res.send('respond with a resource');
 
   // And insert something like this instead:
   res.json([{
