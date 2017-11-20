f2eb327f872773ff4accda1d95349e9603b16f0e
srackley <shelbyrackleyart@gmail.com>
2017-11-20 16:46:39 -0500

    made picture zoomy and abreviated description in case of long

--- client/src/components/SinglePet.jsx
+++ client/src/components/SinglePet.jsx
@@ -36,17 +36,15 @@ class SinglePet extends Component {
         </div>
         <div>
           <h1>{pet.name.$t}</h1>
-          <h2>{(Array.isArray(pet.breeds.breed)
-      ? pet.breeds.breed.map(el => el.$t).join(', ')
-      : pet.breeds.breed.$t
-    )}
-          </h2>
-          <h5>Sex: {pet.sex.$t}</h5>
-          <h3>Age: {pet.age.$t}</h3>
-          <h4>Size: {pet.size.$t}</h4>
+          <h2>{`${pet.age.$t}, ${pet.size.$t === 'L' ? 'Large' : pet.size.$t === 'M' ? 'Medium' : 'Small'}-sized ${pet.sex.$t === 'M' ? ' Male' : ' Female'}`}</h2>
+          <h3>{(Array.isArray(pet.breeds.breed)
+            ? pet.breeds.breed.map(el => el.$t).join(', ')
+            : pet.breeds.breed.$t
+          )}
+          </h3>
         </div>
         <div>
-          <p>{pet.description.$t}</p>
+          <p>{pet.description.$t.length > 600 ? `${pet.description.$t.slice(0, 600)}...` : pet.description.$t}</p>
         </div>
       </div>
     );
