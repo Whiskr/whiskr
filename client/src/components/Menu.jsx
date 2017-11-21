8578488ea0779fe95c6f4a8e2d23dcb273f6b014
srackley <shelbyrackleyart@gmail.com>
2017-11-21 15:40:26 -0500

    putting back in stuff i stupidly deleted

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { logout } from '../store';
 
-const Menu = props => (
-  <nav className="menu">
-    <input type="checkbox" href="#" className="menu-open" name="menu-open" id="menu-open" />
-    <label className="menu-open-button" htmlFor="menu-open">
-      <span className="lines line-1" />
-      <span className="lines line-2" />
-      <span className="lines line-3" />
-    </label>
-    <Link to="/home" className="menu-item item-1" ><FontAwesome name="user" /></Link>
-    <Link to="/matches" className="menu-item item-2"><FontAwesome name="heart" /></Link>
-    <Link to="/pets" className="menu-item item-3" ><FontAwesome name="paw" /></Link>
-    <Link to="/logout" className="menu-item item-4" ><FontAwesome name="sign-out" /></Link>
-  </nav>
-);
-
+const Menu = (props) => {
+  const { handleClick, isLoggedIn } = props;
+  return (
+    <div>
+      {isLoggedIn ?
+        <nav className="menu">
+          <input type="checkbox" href="#" className="menu-open" name="menu-open" id="menu-open" />
+          <label className="menu-open-button" htmlFor="menu-open">
+            <span className="lines line-1" />
+            <span className="lines line-2" />
+            <span className="lines line-3" />
+          </label>
+          <Link to="/home" className="menu-item item-1" ><FontAwesome name="user" /></Link>
+          <Link to="/matches" className="menu-item item-2"><FontAwesome name="heart" /></Link>
+          <Link to="/pets" className="menu-item item-3" ><FontAwesome name="paw" /></Link>
+          <Link to="/logout" className="menu-item item-4" onClick={handleClick} ><FontAwesome name="sign-out" /></Link>
+        </nav>
+: null
+    }
+    </div>
+  );
+};
 
 const mapState = state => ({
   currentUser: state.currentUser,
+  isLoggedIn: !!state.currentUser.id,
 });
+
+const mapDispatch = dispatch => ({
+  handleClick() {
+    dispatch(logout());
+  },
+});
+
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState)(Menu));
+export default withRouter(connect(mapState, mapDispatch)(Menu));
+
+
+Menu.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+};
