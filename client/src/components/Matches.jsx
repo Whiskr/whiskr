767e16cbdcf092d2c1076ebad5fd014dbc068c28
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-21 15:19:28 -0500

    getting matches and match petId

--- client/src/components/Matches.jsx
+++ client/src/components/Matches.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import {fetchMatches} from '../store';
+import React, { Component } from 'react';
+import { fetchMatches } from '../store';
 import { connect } from 'react-redux';
 
 
-const Matches = (props) => {
-    console.log(props.currentUser, "propsz", props.userId)
+class Matches extends Component {
+  componentDidMount() {
+    this.props.onLoad(this.props.currentUser.id);
+  }
+
+  render(){
     return (
-        <div>
+      <div>
         <h1> Matches </h1>
-        <ul>
-        {props.currentUser.matches ? <p>matches</p>
-            : <p>no matches</p>
-        }
+        <ul className='matchesList'>
+          {this.props.matches.length?
+             this.props.matches.map(match =>  {
+            return (<li key={match.id}>{match.petId}</li>)})
+          : <p>NO MATCHES</p>}
         </ul>
-        </div>
+
+      </div>
     )
+  }
 }
 
 
@@ -26,11 +33,12 @@ const Matches = (props) => {
 
 
 const mapState = state => ({
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    matches: state.matches
 })
 
 const mapDispatch = (dispatch) => ({
-    onload(id) {
+    onLoad(id) {
         dispatch(fetchMatches(id));
     }
 })
