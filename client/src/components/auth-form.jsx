4d1e48509643bd41fb2661406b71f33d016f4924
srackley <shelbyrackleyart@gmail.com>
2017-11-23 19:56:18 -0500

    Working flipping from login to sign up

--- client/src/components/auth-form.jsx
+++ client/src/components/auth-form.jsx
@@ -13,62 +13,94 @@ const AuthForm = (props) => {
   let type;
   return (
     <div className="splash">
-      <div className="form animated flipInX">
-        <h1>Whiskr</h1>
-        <h2>Login To Your Account</h2>
-        <form onSubmit={event => handleSubmit(event, type)}>
-          <div>
-            <label htmlFor="email">
-              <small>Email</small>
-            </label>
-            <input name="email" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <button
-              type="submit"
-              onClick={() => {
+      <div className="form animated flipInX login-html">
+        <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked />
+        <label htmlFor="tab-1" className="tab">Log In</label>
+        <input id="tab-2" type="radio" name="tab" className="sign-up" />
+        <label htmlFor="tab-2" className="tab">Sign Up</label>
+        <div className="login-form">
+          <div className="sign-in-htm">
+            <form onSubmit={event => handleSubmit(event, type)}>
+              <div className="group">
+                <label htmlFor="email">
+                  <small>Email</small>
+                </label>
+                <input name="email" type="text" />
+              </div>
+              <div className="group">
+                <label htmlFor="password">
+                  <small>Password</small>
+                </label>
+                <input name="password" type="password" />
+              </div>
+              <div className="group">
+                <button
+                  type="submit"
+                  onClick={() => {
                 type = 'login';
               }}
-            >
+                >
               Log In
-            </button>
-            <button
-              type="submit"
-              onClick={() => {
+                </button>
+
+              </div>
+            </form>
+            <div className="group">
+              <span>or Log in with </span>
+              <a href="/auth/google" >
+                <FontAwesome name="google" className="social google" />
+              </a>
+              <a href="/auth/facebook">
+                <FontAwesome name="facebook" className="social facebook" />
+              </a>
+            </div>
+          </div>
+          <div className="sign-up-htm">
+            <form onSubmit={event => handleSubmit(event, type)}>
+              <div className="group">
+                <label htmlFor="email">
+                  <small>Email</small>
+                </label>
+                <input name="email" type="text" />
+              </div>
+              <div className="group">
+                <label htmlFor="password">
+                  <small>Password</small>
+                </label>
+                <input name="password" type="password" />
+              </div>
+              <div className="group">
+                <label htmlFor="password">
+                  <small>Verify Password</small>
+                </label>
+                <input name="password" type="password" />
+              </div>
+              <div className="group">
+                <button
+                  type="submit"
+                  onClick={() => {
                 type = 'signup';
               }}
-            >
+                >
               Sign Up
-            </button>
+                </button>
+              </div>
+              {/* error && error.response && <div> {error.response.data} </div> */}
+            </form>
+
+            <div>
+              <span>or Sign up with </span>
+              <a href="/auth/google" >
+                <FontAwesome name="google" className="social google" />
+              </a>
+              <a href="/auth/facebook" >
+                <FontAwesome
+                  name="facebook"
+                  className="social facebook"
+                />
+              </a>
+            </div>
           </div>
-          {/* error && error.response && <div> {error.response.data} </div> */}
-        </form>
-        <div>
-          <span>Log in with </span>
-          <a href="/auth/google" >
-            <FontAwesome name="google" className="social google" />
-          </a>
-          <a href="/auth/facebook">
-            <FontAwesome name="facebook" className="social facebook" />
-          </a>
-        </div>
-        <div>
-          <span>Sign up with </span>
-          <a href="/auth/google" >
-            <FontAwesome name="google" className="social google" />
-          </a>
-          <a href="/auth/facebook" >
-            <FontAwesome
-              name="facebook"
-              className="social facebook"
-            />
-          </a>
         </div>
       </div>
     </div>
