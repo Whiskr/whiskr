87f25af463719e0a26e75693cba035b702c1625f
srackley <shelbyrackleyart@gmail.com>
2017-11-25 17:11:51 -0500

    sending emails to hardcoded email with correct info

--- client/src/components/Matches.jsx
+++ client/src/components/Matches.jsx
@@ -1,64 +1,72 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchMatches, fetchPetById } from '../store';
+import { fetchMatches, fetchPetById, sendEmail } from '../store';
 import { connect } from 'react-redux';
 
 class Matches extends Component {
-
   componentDidMount() {
     this.props.onLoad(this.props.currentUser.id);
-    this.props.matches.map(match => {
-      return this.props.onMap(match.petId);
-    })
+    this.props.matches.map(match => this.props.onMap(match.petId));
   }
 
-componentWillMount() {
+  componentWillMount() {
   // reset state here maybe?
-}
+  }
 
-  render(){
+  render() {
     return (
       <div>
         <h1> Matches </h1>
-        <div className='matchesList'>
-            {this.props.matches.length?
-              this.props.matchPets.map(pet => {
-                return(
-                  <div key={pet.id.$t} className='matches petCard'>
-                    <Link to={`/petDetail/${pet.id.$t}`}>
-                    <img src={pet.media.photos.photo[3].$t}/>
+        <div className="matchesList">
+          {this.props.matches.length ?
+              this.props.matchPets.map(pet => (
+                <div key={pet.id.$t} className="matches petCard">
+                  <Link to={`/petDetail/${pet.id.$t}`}>
+                    <img
+                      src={
+            pet.media.photos
+            ? pet.media.photos.photo[3].$t
+            : 'http://biorem.org/wp-content/uploads/2016/07/not-available.png'}
+                      className="petPic rounded"
+                      alt="pet profile pic"
+                    />
                     <h1>{pet.name.$t},{pet.age.$t}</h1>
                     <h2>{pet.breeds.breed.$t},{pet.animal.$t}</h2>
-                    <button> Contact</button>
-                    </Link>
-                  </div>
-                )
-              })
+                    <button onClick={(event) => {
+                      event.preventDefault(); this.props.onClick(this.props.currentUser, pet);
+}}
+                    > Contact
+                    </button>
+                  </Link>
+                </div>
+                ))
               : <p>NO MATCHES!</p>
           }
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapState = state => ({
-    currentUser: state.currentUser,
-    matches: state.matches,
-    matchPets: state.matchPets
-})
+  currentUser: state.currentUser,
+  matches: state.matches,
+  matchPets: state.matchPets,
+});
 
-const mapDispatch = (dispatch) => ({
-    onLoad(id) {
-        dispatch(fetchMatches(id));
-    },
-    onMap(petId) {
-     return (dispatch(fetchPetById(petId)));
-    }
-})
+const mapDispatch = dispatch => ({
+  onLoad(id) {
+    dispatch(fetchMatches(id));
+  },
+  onMap(petId) {
+    return (dispatch(fetchPetById(petId)));
+  },
+  onClick(user, pet) {
+    sendEmail(user, pet);
+  },
+});
 
 export default connect(mapState, mapDispatch)(Matches);
-
 
 
 // mapStatethis.props.matches.map(match =>  {
