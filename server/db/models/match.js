0f127aa4b3db69d272415be5472d0bf9b6c3db1f
Shannon Kendall <snkendall@gmail.com>
2017-11-27 12:02:11 -0500

    merging master

--- server/db/models/match.js
+++ server/db/models/match.js
@@ -1,10 +1,15 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const Match = db.define('match', {
-    petId: {
-        type: Sequelize.INTEGER
-    }
-})
+  petId: {
+    type: Sequelize.INTEGER,
+  },
+  contacted: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 
-module.exports = Match
+});
+
+module.exports = Match;
