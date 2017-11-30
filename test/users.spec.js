b40997668c8a7ee1263d53e41b88361c3c925f20
Shiratie Prodhan <shiratiee@gmail.com>
2017-11-30 12:51:22 -0500

    working on it

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,5 +1,3 @@
-/* global describe beforeEach it */
-
 const {expect} = require('chai')
 const request = require('supertest')
 require('chai').use(require('chai-as-promised'))
@@ -8,67 +6,77 @@ const agent = require('supertest')(require('../server/index'))
 const db = require('../server/db')
 const app = require('../server/index')
 const User = db.model('user')
+const Match= db.model('match')
 
-    
 describe('User routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
 
-  describe('/api/users/:id', () => {
-    const catLadyEmail = 'catlady@gmal.com'
-    const password = 'catscatscats'
+  beforeEach(() => {
+    return db.sync({ force: true })
+  });
+
+  describe('/api/users', () => {
 
     beforeEach(() => {
       return User.create({
-        email: catLadyEmail,
-        password: password
+        email: 'cat@gmail.com',
+        password: 'cat'
+      })
+      .then(newUser => {
+        return User.create({
+          email: 'dog@gmail.com',
+          password: 'god'
+        })
       })
     })
 
-    it('GET /api/users/:id', () => {
-      return request(app)
-        .get('/api/users/${id}')
+     it('GET /api/user/:id shows a single user', (done) => {
+      request(app)
+        .get('/api/users/2')
         .expect(200)
         .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(catLadyEmail)
+          expect(res.body).to.be.an('object')
+          expect(res.body.id).to.equal(2);
+          expect(res.body.name).to.equal('Speck');
+          done();
         })
     })
-    it('returns a 404 error if the ID is not correct', function () {
-      
-            return request(app)
-            .get('/users/76146')
-            .expect(404);
-      
-          });
-    it('updates a user at PUT', () => {
-      return request(app)
-          .put(`/api/users/${id}`)
-          .send({
-              email: 'catlady@hotmail.com'
-          })
-          .expect(201)
-          .then(res => {
-              return User.findById(catlady);
-          })
-          .then(user => {
-              expect(user.email).to.be.equal('catlady@gmail.com');
-          });
+
+    it('GET /api/cats/:id return null if no cat id is passed in', (done) => {
+      request(app)
+        .get('/api/cats/34')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.null;
+          done();
+        })
     })
-    it('deletes a user at Delete', () => {
-      return request(app)
-          .delete(`/api/users/${catlady}`)
-          .send({
-              email: 'catlady@hotmail.com'
-          })
-          .expect(201)
-          .then(res => {
-              return User.findById(catlady);
-          })
-          .then(user => {
-              expect(user.email).to.be.equal('catlady@gmail.com');
-          });
-    })
-  }) 
-});
+
+    it('PUT /api/cats/:id updates a cats information', () => {
+        return agent
+        .put('/api/cats/2')
+        .send({
+            password:eat
+        })
+        .expect(200)
+        .expect(function(res) {
+            expect(res.body.updatedResponse.id).to.not.be.an('undefined');
+            expect(res.body.updatedResponse.gender).to.equal('male');
+            expect(res.body.updatedResponse.status).to.equal('adopted');
+        });
+     })
+
+    it('DELETE /api/cats/:id removes a cat', () => {
+        return agent
+        .delete('/api/cats/1')
+        .expect(200)
+        .then(res => {
+            User.findById(1)
+            .then(function(destroyed) {
+            expect (destroyed).to.be.null;
+            })
+        })
+        })
+
+
+  })
+})
