2fbe84282eb2fef222f2341f5b70caf1489e2056
Shannon Kendall <snkendall@gmail.com>
2017-11-30 10:52:18 -0500

    bug patched

--- client/src/components/Matches.jsx
+++ client/src/components/Matches.jsx
@@ -13,9 +13,10 @@ class Matches extends Component {
         <div className="matchesList">
           {this.props.matches.length ?
               this.props.matchPets.map(pet => { 
-                const contacted = this.props.matches.filter(match => match.petId === Number(pet.id.$t))[0].contacted
+                const contacted = pet.id && this.props.matches.filter(match => match.petId === Number(pet.id.$t))[0].contacted
                 console.log(contacted)
-                return (
+                return ( 
+                pet.id &&
                 <div key={pet.id.$t} className="matches petCard">
                   <Link to={`matches/${pet.id.$t}`}>
                     <img
